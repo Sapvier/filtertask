e7241278303e580c18ea3b648d36f62ec5049caa
Denys Zheleznyi <denys.zheleznyi@chisw.com>
2021-05-20 18:17:40 +0300

    main, material-ui changed to react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import FilterItem from "./FilterItem";
 import {filters} from "./constants";
-import {useState} from "react";
+import React, {useState} from "react";
+import {Accordion, Card, Form} from "react-bootstrap";
 
 function App() {
     const [fil, setFil] = useState([])
@@ -8,13 +8,28 @@ function App() {
     const filHandler = (e) => {
         setFil(prevState => [...prevState, e.target.value])
     }
-    console.log(fil)
+
 
     return (
         <div className="app">
-            <div className="filter-box">
-                {filters.map(filter => <FilterItem key={filter.id} filHandler={filHandler} filter={filter}/>)}
-            </div>
+            <Accordion>
+                {filters.map(filter =>
+                    <Card key={filter.id} style={{width: '250px'}}>
+                        <Card.Header >
+                            <Accordion.Toggle eventKey={filter.id + ""} style={{border: `none`, backgroundColor: 'rgba(0,0,0,.005)'}}>
+                                {filter.title}
+                            </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey={filter.id + ""}>
+                            <Card.Body>
+                                <Form.Group controlId="formBasicCheckbox">
+                                    {filter.values.map(item => <Form.Check key={item.id} onChange={filHandler} type="checkbox" label={item.value} value={item.id} />)}
+                                </Form.Group>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                )}
+            </Accordion>
         </div>
     );
 }
--- src/FilterItem.jsx
+++ src/FilterItem.jsx
@@ -1,44 +1,45 @@
-import React, {useState} from 'react';
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import {Checkbox, FormControlLabel, FormGroup} from "@material-ui/core";
-
-
-const FilterItem = ({filter, filHandler}) => {
-    const [expanded, setExpanded] = useState(false);
-    const [checked, setChecked] = useState(false)
-
-    const handleChange = () => {
-        setExpanded(prevState => !prevState);
-    };
-    const handleCheckChange = (event) => {
-        filHandler(event)
-        setChecked(event.target.checked);
-    };
-
-
-    return (
-        <div>
-            <Accordion square expanded={expanded} onChange={handleChange}>
-                <AccordionSummary aria-controls="panel1d-content" id="panel1d-header"
-                                  style={{border: "1px solid grey"}}>
-                    <p style={{textTransform: 'capitalize'}}>{filter.title}</p>
-                </AccordionSummary>
-                <AccordionDetails style={{border: "1px solid grey"}}>
-                    <FormGroup>
-                        {filter.values.map(value => (
-                            <FormControlLabel key={value.id}
-                                              control={<Checkbox onChange={handleCheckChange} value={value.id}/>}
-                                              label={value.value}
-                            />
-                        ))}
-                    </FormGroup>
-                </AccordionDetails>
-            </Accordion>
-        </div>
-    )
-};
-
-
-export default FilterItem;
+// import React, {useState} from 'react';
+// import {Accordion, Card, Button} from "react-bootstrap";
+//
+//
+// const FilterItem = ({filter, filHandler}) => {
+//     const [expanded, setExpanded] = useState(false);
+//     const [checked, setChecked] = useState(false)
+//
+//     const handleChange = () => {
+//         setExpanded(prevState => !prevState);
+//     };
+//     const handleCheckChange = (event) => {
+//         filHandler(event)
+//         setChecked(event.target.checked);
+//     };
+//
+//
+//     return (
+//         <Accordion defaultActiveKey="0">
+//             <Card>
+//                 <Card.Header>
+//                     <Accordion.Toggle as={Button} variant="link" eventKey="0">
+//                         Click me!
+//                     </Accordion.Toggle>
+//                 </Card.Header>
+//                 <Accordion.Collapse eventKey="0">
+//                     <Card.Body>Hello! I'm the body</Card.Body>
+//                 </Accordion.Collapse>
+//             </Card>
+//             <Card>
+//                 <Card.Header>
+//                     <Accordion.Toggle as={Button} variant="link" eventKey="1">
+//                         Click me!
+//                     </Accordion.Toggle>
+//                 </Card.Header>
+//                 <Accordion.Collapse eventKey="1">
+//                     <Card.Body>Hello! I'm another body</Card.Body>
+//                 </Accordion.Collapse>
+//             </Card>
+//         </Accordion>
+//     )
+// };
+//
+//
+// export default FilterItem;
